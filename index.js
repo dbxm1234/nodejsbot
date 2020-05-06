49e0310acb1382c160b52b2df2d5dc2644a71cec
dbxm1234 <dbxm12345@gmail.com>
2020-05-07 04:42:15 +0900

    add clear command

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ client.on("guildMemberRemove", (member) => {
 client.on('message', (message) => {
   if(message.author.bot) return;
 
-  if(message.content == 'ping') {
-    return message.reply('pong');
+  if(message.content == '굿모닝') {
+    return message.reply('좋은아침이에요 활기찬하루 돼세요 ~!');
   }
 
   if(message.content == 'embed') {
